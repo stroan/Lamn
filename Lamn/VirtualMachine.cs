91f9fc34344726e59902fbe54d4ea7275d20b26a
Stephen Roantree <stroantree@gmail.com>
2011-10-04 23:32:47 +0100

    Merged call stack into main evaluation stack.

--- Lamn/VirtualMachine.cs
+++ Lamn/VirtualMachine.cs
@@ -10,15 +10,21 @@ namespace Lamn
 		public class OpCodes
 		{
 			public const UInt32 LOADK = 0x01000000;
-			public const UInt32 ADD = 0x02000000;
-			public const UInt32 RET = 0x03000000;
+			public const UInt32 ADD   = 0x02000000;
+			
+			public const UInt32 RET   = 0x03000000;
+			public const UInt32 CALL  = 0x07000000;
+
+			public const UInt32 NEWVARG  = 0x04000000;
+			public const UInt32 PUSHVARG = 0x05000000;
+			public const UInt32 POPVARG  = 0x06000000;
 
 			public const UInt32 OPCODE_MASK = 0xFF000000;
-			public const UInt32 OP1_MASK = 0x00FFF000;
-			public const UInt32 OP2_MASK = 0x00000FFF;
-			public const int OPCODE_SHIFT = 24;
-			public const int OP1_SHIFT = 12;
-			public const int OP2_SHIFT = 0;
+			public const UInt32 OP1_MASK    = 0x00FFF000;
+			public const UInt32 OP2_MASK    = 0x00000FFF;
+			public const int OPCODE_SHIFT   = 24;
+			public const int OP1_SHIFT      = 12;
+			public const int OP2_SHIFT      = 0;
 
 			public static UInt32 MakeLoadConstant(int index)
 			{
@@ -37,6 +43,16 @@ namespace Lamn
 				Bytecodes = bytecodes;
 				Constants = constants;
 				Index = index;
+			}
+		}
+
+		public class VarArgs
+		{
+			public LinkedList<Object> Args { get; private set; }
+
+			public VarArgs()
+			{
+				Args = new LinkedList<Object>();	
 			}
 		}
 
@@ -66,19 +82,27 @@ namespace Lamn
 			}
 		}
 
+		public class StackCell
+		{
+			public Object contents;
+		}
+
 		public class VMException : Exception { }
 
 		private List<Function> FunctionMap { get; set; }
 
-		private List<Object> Stack { get; set; }
-		private Stack<InstructionPointer> CallStack { get; set; }
+		private StackCell[] Stack { get; set; }
 		private InstructionPointer CurrentIP { get; set; }
+
+		private const int stackSize = 512;
+
+		private int baseIndex = 0;
+		private int stackIndex = 0;
 
 		public VirtualMachine()
 		{
 			FunctionMap = new List<Function>();
-			Stack = new List<Object>();
-			CallStack = new Stack<InstructionPointer>();
+			Stack = new StackCell[stackSize];
 		}
 
 		public int RegisterFunction(UInt32[] bytecodes, Object[] constants)
@@ -105,6 +129,9 @@ namespace Lamn
 					case OpCodes.RET:
 						DoRET();
 						break;
+					case OpCodes.CALL:
+						DoCALL();
+						break;
 					default:
 						throw new VMException();
 				}
@@ -113,39 +140,26 @@ namespace Lamn
 
 		public void Call()
 		{
-			Object o = PopStack();
-
-			InstructionPointer newIP = null;
-
-			if (o is Closure)
-			{
-				Function f = FunctionMap[((Closure)o).FunctionIndex];
-				newIP = new InstructionPointer(f, 0);
-			}
-			else
-			{
-				throw new VMException();
-			}
-
-			CallStack.Push(CurrentIP);
-			CurrentIP = newIP;
+			DoCALL();
 		}
 
 		#region Manipulate Stacks
 		public void PushStack(Object o) {
-			Stack.Add(o);
+			Stack[stackIndex] = new StackCell() { contents = o };
+			stackIndex++;
 		}
 
 		public Object PopStack()
 		{
-			Object retValue = GetStack(0);
-			Stack.RemoveAt(Stack.Count - 1);
+			stackIndex--;
+			Object retValue = Stack[stackIndex].contents;
+			Stack[stackIndex] = null;
 			return retValue;
 		}
 
-		public Object GetStack(int index)
+		public Object GetStackAtIndex(int index)
 		{
-			return Stack[Stack.Count - (index + 1)];
+			return Stack[index].contents;
 		}
 		#endregion
 
@@ -167,11 +181,43 @@ namespace Lamn
 
 		private void DoRET()
 		{
-			CurrentIP = CallStack.Pop();
+			int oldBaseIndex = (int)GetStackAtIndex(baseIndex);
+
+			while (stackIndex > baseIndex)
+			{
+				PopStack();
+			}
+
+			baseIndex = oldBaseIndex;
+
+			CurrentIP = (InstructionPointer)PopStack();
 			if (CurrentIP != null)
 			{
 				CurrentIP.InstructionIndex++;
 			}
+		}
+
+		private void DoCALL()
+		{
+			Object o = PopStack();
+
+			InstructionPointer newIP = null;
+
+			if (o is Closure)
+			{
+				Function f = FunctionMap[((Closure)o).FunctionIndex];
+				newIP = new InstructionPointer(f, 0);
+			}
+			else
+			{
+				throw new VMException();
+			}
+
+			PushStack(CurrentIP);
+			PushStack(baseIndex);
+			baseIndex = stackIndex - 1;
+
+			CurrentIP = newIP;
 		}
 		#endregion
 	}
