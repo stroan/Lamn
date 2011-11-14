84bf32e61189950526fd5083a98c728fe6686d90
Stephen Roantree <stroantree@gmail.com>
2011-11-14 00:34:57 +0000

    Add portable project for use with xbox, windows phone, etc.

--- Lamn/Properties/AssemblyInfo.cs
+++ Lamn/Properties/AssemblyInfo.cs
@@ -17,10 +17,10 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("15cd8697-5f0b-40be-bd74-29dd8ccafce7")]
+//[assembly: Guid("15cd8697-5f0b-40be-bd74-29dd8ccafce7")]
 
 // Version information for an assembly consists of the following four values:
 //
